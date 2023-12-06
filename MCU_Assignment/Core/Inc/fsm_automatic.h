9649e7f0a63a2200568baf498ff828d45838bf91
MSI\nvtie <tien.ngo30082002@hcmut.edu.vn>
2023-12-06 22:20:38 +0700

    automatic.c, automatic.h, scheduler.c, scheduler.h

---
+++ MCU_Assignment/Core/Inc/fsm_automatic.h
@@ -0,0 +1,9 @@
+
+#ifndef INC_FSM_AUTOMATIC_H_
+#define INC_FSM_AUTOMATIC_H_
+
+#include "global.h"
+
+void fsm_automatic_run();
+
+#endif /* INC_FSM_AUTOMATIC_H_ */
--- MCU_Assignment/Debug/Core/Src/subdir.mk
+++ MCU_Assignment/Debug/Core/Src/subdir.mk
@@ -5,6 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Core/Src/fsm_automatic.c \
 ../Core/Src/global.c \
 ../Core/Src/main.c \
 ../Core/Src/stm32f1xx_hal_msp.c \
@@ -14,6 +15,7 @@ C_SRCS += \
 ../Core/Src/system_stm32f1xx.c 
 
 OBJS += \
+./Core/Src/fsm_automatic.o \
 ./Core/Src/global.o \
 ./Core/Src/main.o \
 ./Core/Src/stm32f1xx_hal_msp.o \
@@ -23,6 +25,7 @@ OBJS += \
 ./Core/Src/system_stm32f1xx.o 
 
 C_DEPS += \
+./Core/Src/fsm_automatic.d \
 ./Core/Src/global.d \
 ./Core/Src/main.d \
 ./Core/Src/stm32f1xx_hal_msp.d \
