c3273c1f157fb4bdc107da57721b1d7cd2d3402a
DESKTOP-6PB2JMV\Phuc Toan <phuctoan0708@gmail.com>
2023-12-07 15:39:41 +0700

    update pedestrian

--- MCU_Assignment/Core/Inc/fsm_pedestrian.h
+++ MCU_Assignment/Core/Inc/fsm_pedestrian.h
@@ -8,6 +8,10 @@
 #ifndef INC_FSM_PEDESTRIAN_H_
 #define INC_FSM_PEDESTRIAN_H_
 
+#include "global.h"
+
+void toggle_buzzer(int volume);
+void buzzer_run();
 void fsm_pedestrian_run();
 
 #endif /* INC_FSM_PEDESTRIAN_H_ */
