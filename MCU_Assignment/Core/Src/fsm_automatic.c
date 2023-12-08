4afa6dff6f9149df50fe518f1762dfd879f06806
MSI\nvtie <tien.ngo30082002@hcmut.edu.vn>
2023-12-08 14:25:14 +0700

    update automatic.c

--- MCU_Assignment/Core/Src/fsm_automatic.c
+++ MCU_Assignment/Core/Src/fsm_automatic.c
@@ -1,5 +1,22 @@
+
 #include "fsm_automatic.h"
-int index_led = 0;
+
+void updateLightTime(){
+	if(timer_flag[0] == 1){
+		setTimer(1000, 0);
+		if(sec1 > 0) sec1--;
+		if(sec2 > 0) sec2--;
+	}
+}
+
+void ifButton1Pressed(){
+	if(isButtonPressed(0) == 1){
+		status = MAN_RED;
+		setTimer(250, 2);
+		clearAllLED();
+	}
+}
+
 void fsm_automatic_run(){
 	switch(status){
 	case INIT:
@@ -8,88 +25,66 @@ void fsm_automatic_run(){
 
 		sec1 = red;
 		sec2 = green;
+
+		updateClockBuffer(sec1, sec2);
 		break;
 	case RED1_GREEN2:
 		turnonTrafficLight1(RED);
 		turnonTrafficLight2(GREEN);
 
-		if(timer_flag[0] == 1){
-			setTimer(1000, 0);
-			if(sec1 > 0) sec1--;
-			if(sec2 > 0) sec2--;
-		}
+		updateClockBuffer(sec1, sec2);
+		updateLightTime();
 
 		if(sec2 <= 0){
 			status = RED1_YELLOW2;
 			sec2 = yellow;
 		}
-		if(isButtonPressed(0) == 1){
-			status = MAN_RED;
-			setTimer(250, 2);
-			clearAllLED();
-		}
+
+		ifButton1Pressed();
 		break;
 	case RED1_YELLOW2:
 		turnonTrafficLight1(RED);
 		turnonTrafficLight2(YELLOW);
 
-		if(timer_flag[0] == 1){
-			setTimer(1000, 0);
-			if(sec1 > 0) sec1--;
-			if(sec2 > 0) sec2--;
-		}
+		updateClockBuffer(sec1, sec2);
+		updateLightTime();
 
 		if(sec2 <= 0){
 			status = GREEN1_RED2;
 			sec1 = green;
 			sec2 = red;
 		}
-		if(isButtonPressed(0) == 1){
-			status = MAN_RED;
-			setTimer(250, 2);
-			clearAllLED();
-		}
+
+		ifButton1Pressed();
 		break;
 	case GREEN1_RED2:
 		turnonTrafficLight1(GREEN);
 		turnonTrafficLight2(RED);
 
-		if(timer_flag[0] == 1){
-			setTimer(1000, 0);
-			if(sec1 > 0) sec1--;
-			if(sec2 > 0) sec2--;
-		}
+		updateClockBuffer(sec1, sec2);
+		updateLightTime();
 
 		if(sec1 <= 0){
 			status = YELLOW1_RED2;
 			sec1 = yellow;
 		}
-		if(isButtonPressed(0) == 1){
-			status = MAN_RED;
-			setTimer(250, 2);
-			clearAllLED();
-		}
+
+		ifButton1Pressed();
 		break;
 	case YELLOW1_RED2:
 		turnonTrafficLight1(YELLOW);
 		turnonTrafficLight2(RED);
 
-		if(timer_flag[0] == 1){
-			setTimer(1000, 0);
-			if(sec1 > 0) sec1--;
-			if(sec2 > 0) sec2--;
-		}
+		updateClockBuffer(sec1, sec2);
+		updateLightTime();
+
 		if(sec1 <= 0){
 			status = RED1_GREEN2;
 			sec1 = red;
 			sec2 = green;
 		}
-		if(isButtonPressed(0) == 1){
-			status = MAN_RED;
-			setTimer(250, 2);
-			clearAllLED();
-		}
-		break;
+
+		ifButton1Pressed();
 	default:
 		break;
 	}
